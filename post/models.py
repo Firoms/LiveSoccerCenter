f3eaaab93ccb98934aad30f58d96ec2d21e62047
Firoms <edisonmessi@naver.com>
2021-03-31 07:33:46 +0900

    model 작성& views 작성 시작

--- post/models.py
+++ post/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=80, null=False)
+    content = models.TextField(null=False)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
+    delete = models.BooleanField(default=False)
+    files = models.FileField(upload_to='uploads/%Y/%m/%d/', max_length=None, null=True, blank=True) # None 안될 시 100
+
+    def __str__(self):
+        return f"{self.id}. {self.title}"
+    
+
+class Comment(models.Model):
+    content = models.CharField(max_length=80, null=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
+    delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}. {self.content}"
+
--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    # index_datas = Post.objects.filter(delete=False).order_by("-pub_date")
+    # context = {"Post_data" : index_datas}
+    print("index 호출")
+    return HttpResponse("hello")
