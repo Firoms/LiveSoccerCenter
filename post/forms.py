b338e889cc94e7270b32b13249c44be6c5f9e535
Firoms <edisonmessi@naver.com>
2021-06-21 07:46:23 +0900

    BETA V.0.8

--- post/forms.py
+++ post/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 class AddForm(forms.Form):
     title = forms.CharField(label="제목", max_length=30)
     content = forms.CharField(label="내용")
-    files = forms.FileField(label="파일")
+    files = forms.FileField(label="파일", required=False)
 
 
 # class AddForm(forms.ModelForm):
